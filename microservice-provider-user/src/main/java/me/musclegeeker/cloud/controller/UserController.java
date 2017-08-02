a927086d05e226b58f001291187c5d3d1b38cf8d
lianghj <lianghj@citic.com>
2017-08-02 12:42:24 +0800

    添加客户端侧负载均衡Ribbon

--- microservice-provider-user/src/main/java/me/musclegeeker/cloud/controller/UserController.java
+++ microservice-provider-user/src/main/java/me/musclegeeker/cloud/controller/UserController.java
@@ -1,7 +1,11 @@
 package me.musclegeeker.cloud.controller;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import me.musclegeeker.cloud.domain.User;
 import me.musclegeeker.cloud.repository.UserRepository;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +18,25 @@ public class UserController {
     @Resource
     private UserRepository userRepository;
 
+    @Resource
+    private EurekaClient eurekaClient;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/simple/user/{id}")
     public User findUserById(@PathVariable Long id) {
         return userRepository.findOne(id);
+    }
+
+    @GetMapping("/eureka-instance")
+    public String serviceUrl() {
+        InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
+        return instance.getHomePageUrl();
+    }
+
+    @GetMapping("/instance-info")
+    public ServiceInstance instanceInfo() {
+        return this.discoveryClient.getLocalServiceInstance();
     }
 }
