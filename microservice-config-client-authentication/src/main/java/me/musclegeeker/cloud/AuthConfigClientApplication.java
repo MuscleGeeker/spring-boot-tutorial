8854a662341cdabb438ca95f2693c9a238790b15
lianghj <lianghj@citic.com>
2017-09-07 15:17:35 +0800

    cloud config authentication

---
+++ microservice-config-client-authentication/src/main/java/me/musclegeeker/cloud/AuthConfigClientApplication.java
@@ -0,0 +1,11 @@
+package me.musclegeeker.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthConfigClientApplication.class, args);
+    }
+}
