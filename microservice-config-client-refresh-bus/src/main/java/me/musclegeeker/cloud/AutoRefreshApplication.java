e85bb60e26a23522efce5a5cff5f2d072238266c
lianghj <lianghj@citic.com>
2017-09-12 15:25:07 +0800

    config server的自动刷新

---
+++ microservice-config-client-refresh-bus/src/main/java/me/musclegeeker/cloud/AutoRefreshApplication.java
@@ -0,0 +1,12 @@
+package me.musclegeeker.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoRefreshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoRefreshApplication.class, args);
+    }
+}
